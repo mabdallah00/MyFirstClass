6f8e626fcaf2f4c57cee9f25973ca5ccd2c687b6
mabdallah00 <mabdallah1@my.dom.edu>
2025-09-29 17:52:31 -0500

    Update party_event.py

--- party_event.py
+++ party_event.py
@@ -3,8 +3,8 @@ from interfaces import Printable
 
 class PartyEvent(Event, Printable):
     def __init__(self, name, date, time, location, theme, guest_list):
-        super().__init__(name, date, time, location, theme)
-        self.guest_count = guest_list
+        super().__init__(name, date, time, location, theme, guest_list)
+        self.guest_list = guest_list
 
     def printProperties(self):
         print("=== Party Event Properties ===")
